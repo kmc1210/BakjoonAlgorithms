0c94cac133f4592b2ded7d30c7636f3c28652e90
kmc1210 <skmnzn@gmail.com>
2020-08-27 19:51:43 +0900

    백준 11943번 문제 [java]

---
+++ Quiz/100_Challenge/Bake_11943.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt(); //사과
+		int b=sc.nextInt(); //오렌지
+		int c=sc.nextInt();//사과
+		int d=sc.nextInt();//오렌지
+		
+		System.out.println(a+d>b+c?b+c:a+d);
+		
+	}
+	
+}
