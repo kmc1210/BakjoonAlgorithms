175492e15cde8942e76fdcf63b2c77ce93bb4f56
kmc1210 <skmnzn@gmail.com>
2020-09-12 23:47:48 +0900

    Baek_10817-세 수

---
+++ Quiz/100_Challenge/Baek_10817.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			int[] arr = new int[3];
+			for(int i=0; i<arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+			System.out.println(arr[1]);
+	}
+}
